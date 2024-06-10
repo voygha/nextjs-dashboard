84d940f503a8af00c8adb343cb7afe980d187c9b
voygha <joseluiscastanonalvarado@gmail.com>
2024-06-10 15:25:01 -0600

    Improving Accessibility

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,4 +1,7 @@
 'use client';
+import { useFormState } from "react-dom";
+
+
 import { updateInvoice } from '@/app/lib/actions';
 
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
@@ -18,10 +21,13 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const initialState = { message: null, errors: {} };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  const [state, dispatch] = useFormState(updateInvoiceWithId, initialState);
+
   return (
     
-    <form action={updateInvoiceWithId}>
+    <form action={dispatch}>
       <input type="hidden" name="id" value={invoice.id} />
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
